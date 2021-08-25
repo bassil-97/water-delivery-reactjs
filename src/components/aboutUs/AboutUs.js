a2b4395ded9add2f7dcda681d3b9ef0f2cdd67fe
root <bassilalqadi.ba@gmail.com>
2021-08-25 23:46:17 +0300

    modifications

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './AboutUs.css';
 
+import AboutUsElement from './aboutUsElement/AboutUsElement';
+
 export default function AboutUs() {
     return (
         <div className="about__us">
@@ -23,70 +25,24 @@ export default function AboutUs() {
                 and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
                 <div className="about__us__elements">
-                    <div className="about__us_element">
-                        <div className="about__us__icon">
-                            <lottie-player 
-                                src="https://assets9.lottiefiles.com/packages/lf20_7UVfnn.json"  
-                                background="transparent"  speed="1"  style={{width: "60px", height: "60px"}}  
-                                loop  autoplay
-                            />
-                        </div>
-                        <span>Hosha, Jordan</span>
-                    </div>
-                    <div className="about__us_element">
-                        <div className="about__us__icon">
-                            <lottie-player 
-                                src="https://assets4.lottiefiles.com/packages/lf20_ebqz3ltq.json"  
-                                background="transparent"  speed="1"  style={{width: "90px", height: "90px"}}  
-                                loop  autoplay
-                            />
-                        </div>
-                        <span>bassilalqadi65@gmail.com</span>
-                    </div>
-                    <div className="about__us_element">
-                        <div className="about__us__icon">
-                            <lottie-player 
-                                src="https://assets5.lottiefiles.com/private_files/lf30_GNPF2n.json"  
-                                background="transparent"  speed="1"  style={{width: "100px", height: "100px"}}  
-                                loop  autoplay
-                            />
-                        </div>
-                        <span>+962791858735</span>
-                    </div>
-                    
-                </div>
-                <div className="about__us__elements">
-                    <div className="about__us_element">
-                        <div className="about__us__icon">
-                            <lottie-player 
-                                src="https://assets9.lottiefiles.com/packages/lf20_7UVfnn.json"  
-                                background="transparent"  speed="1"  style={{width: "60px", height: "60px"}}  
-                                loop  autoplay
-                            />
-                        </div>
-                        <span>Hosha, Jordan</span>
-                    </div>
-                    <div className="about__us_element">
-                        <div className="about__us__icon">
-                            <lottie-player 
-                                src="https://assets4.lottiefiles.com/packages/lf20_ebqz3ltq.json"  
-                                background="transparent"  speed="1"  style={{width: "90px", height: "90px"}}  
-                                loop  autoplay
-                            />
-                        </div>
-                        <span>bassilalqadi65@gmail.com</span>
-                    </div>
-                    <div className="about__us_element">
-                        <div className="about__us__icon">
-                            <lottie-player 
-                                src="https://assets5.lottiefiles.com/private_files/lf30_GNPF2n.json"  
-                                background="transparent"  speed="1"  style={{width: "100px", height: "100px"}}  
-                                loop  autoplay
-                            />
-                        </div>
-                        <span>+962791858735</span>
-                    </div>
-                    
+                    <AboutUsElement 
+                        IconSrc={"https://assets9.lottiefiles.com/packages/lf20_7UVfnn.json"}
+                        width={"60px"}
+                        height={"60px"}
+                        content={"Hosha, Jordan"}
+                    />
+                    <AboutUsElement 
+                        IconSrc={"https://assets4.lottiefiles.com/packages/lf20_ebqz3ltq.json"}
+                        width={"90px"}
+                        height={"90px"}
+                        content={"bassilalqadi65@gmail.com"}
+                    />
+                    <AboutUsElement 
+                        IconSrc={"https://assets5.lottiefiles.com/private_files/lf30_GNPF2n.json"}
+                        width={"100px"}
+                        height={"100px"}
+                        content={"+962791858735"}
+                    />
                 </div>
             </div>
         </div>
---
+++ src/components/aboutUs/aboutUsElement/AboutUsElement.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AboutUsElement.css';
+
+export default function AboutUsElement(props) {
+    return (
+        <div className="about__us_element">
+            <div className="about__us__icon">
+                <lottie-player 
+                    src={props.IconSrc}
+                    background="transparent"  speed="1"  style={{width: props.width, height: props.height}}  
+                    loop  autoplay
+                />
+            </div>
+            <span>{props.content}</span>
+        </div>
+    )
+}
